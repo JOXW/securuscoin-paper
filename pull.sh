45e141f8c27cc986e5b41ee696805e6979489b75
RockSteady <34389545+RocksteadyTC@users.noreply.github.com>
2018-01-05 21:49:55 -0600

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,12 @@
+######################################
+#!/usr/bin/env bash
+# For display purposes only.
+# If you use this script, you'll die.
+######################################
+
+cd /var/www/html/wallet # make sure we're being called in the right place
+rm -rf /var/www/html/wallet/* # lol yolo
+git clone https://github.com/turtlecoin/paper-turtle . # this would have been git pull, but isn't
+echo COMPLETE # bet you didnt know you can echo without quotes
+
+# comment
