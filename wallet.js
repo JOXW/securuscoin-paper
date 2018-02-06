59d02f29e3db6a457bab64a0e59a13005ca398bb
root <root@vultr.guest>
2018-02-06 08:14:11 +0000

    per monero docs for electrum/mnemnonic style keys, it's expected that the seed matches the spend key

--- wallet.js
+++ wallet.js
@@ -16,7 +16,6 @@ var getStringWords = function(string) {
 
 var genkeys = function(additional_entropy, lang) {
   var seed = cnUtil.sc_reduce32(poor_mans_kdf(additional_entropy + cnUtil.rand_32()));
-  seed = CryptoJS.SHA256(salt + seed).toString();
   var keys = cnUtil.create_address(seed);
   var passPhrase = mn_encode(seed, lang);
   return {
